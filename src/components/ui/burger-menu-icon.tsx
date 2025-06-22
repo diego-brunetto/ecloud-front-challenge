95209bc847486b8fe46fff4b29fc421ffc03ca12
Diego <diego.brunetto2@gmail.com>
2025-06-22 20:54:33 -0300

    feat: add burger icon component

---
+++ src/components/ui/burger-menu-icon.tsx
@@ -0,0 +1,30 @@
+interface BurgerMenuIconProps {
+  width?: string | number;
+  height?: string | number;
+  className?: string;
+  color?: string;
+}
+
+const BurgerMenuIcon = ({ 
+  width = 24, 
+  height = 24, 
+  className,
+  color = "#3D63A9" 
+}: BurgerMenuIconProps) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path d="M4 6H20V8H4V6Z" fill={color} />
+      <path d="M4 11H20V13H4V11Z" fill={color} />
+      <path d="M20 16H10V18H20V16Z" fill={color} />
+    </svg>
+  );
+};
+
+export default BurgerMenuIcon; 
