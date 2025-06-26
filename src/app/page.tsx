0481e15b3d261e376020257eeccbd2caca961ec8
Diego <diego.brunetto2@gmail.com>
2025-06-25 21:02:57 -0300

    feat: add section to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Hero from "@/components/sections/hero";
+import HowItWorks from "@/components/sections/how-it-works";
+import Services from "@/components/sections/services";
+import { Flex } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <div>
-      {/* Homepage content will go here */}
-    </div>
+    <Flex flexDirection="column">
+      <Hero />
+      <HowItWorks />
+      <Services />
+    </Flex>
   );
 }
