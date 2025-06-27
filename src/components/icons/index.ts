3e8d9d27039b62fef706c5a5edcad3361d1b74a5
Diego <diego.brunetto2@gmail.com>
2025-06-26 21:11:58 -0300

    feat: add social icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,3 +2,7 @@ export { default as BurgerMenuIcon } from './burger-menu-icon';
 export { default as CalendarIcon } from './calendar-icon';
 export { default as LogoIcon } from './logo-icon'; 
 export { default as LocationIcon } from './location-icon';
+export { default as FacebookIcon } from './socials/facebook-icon';
+export { default as InstagramIcon } from './socials/instagram-icon';
+export { default as TwitterIcon } from './socials/twitter-icon';
+export { default as YoutubeIcon } from './socials/youtube-icon';
